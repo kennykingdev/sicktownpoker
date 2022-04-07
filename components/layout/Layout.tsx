1deee66b02c22c8b02ae085249a4878e6280485c
Kenny King <kenny@kennyking.dev>
2022-04-06 21:07:37 -0700

    Create basic layout

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Image, Stack, Text, Spacer, Link } from '@chakra-ui/react';
+import { FC } from 'react';
+import NextLink from 'next/link';
+import 'focus-visible/dist/focus-visible';
+
+const Layout: FC = ({ children }) => {
+	return (
+		<>
+			<Flex direction={['column', 'row']} maxW='full' h='100vh' p={0}>
+				<Box id='navbar' bg='gray.800' color='whiteAlpha.800' minW='150px'>
+					<Flex direction={['row', 'column']} h='100%'>
+						<Image alt='logo' boxSize='80px' src='/favicon.ico' p={2} />
+						<Stack direction={['row', 'column']}>
+							<NextLink href='/' passHref>
+								<Link>Home</Link>
+							</NextLink>
+							<NextLink href='/players' passHref>
+								<Link>Players</Link>
+							</NextLink>
+						</Stack>
+						<Spacer />
+						<Stack direction={['row', 'column']}>
+							<Text>Login</Text>
+							<Text>Logout</Text>
+						</Stack>
+					</Flex>
+				</Box>
+
+				<Box id='content'>{children}</Box>
+			</Flex>
+		</>
+	);
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider resetCSS={true}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
