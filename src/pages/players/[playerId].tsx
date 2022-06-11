dbae672dc39964bba41d886aa9042d7a85b4de1c
Kenny King <kenny@kennyking.dev>
2022-06-11 16:15:37 -0700

    Add delete player operation

--- src/pages/players/[playerId].tsx
+++ src/pages/players/[playerId].tsx
@@ -1,10 +1,12 @@
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { dehydrate, QueryClient } from 'react-query';
+import { dehydrate, QueryClient, useQueryClient } from 'react-query';
 import { ParsedUrlQuery } from 'querystring';
 import {
   useGetPlayerDetailsQuery,
   GetPlayerDetailsQuery,
+  useDeletePlayerMutation,
+  usePlayersIndexQuery,
 } from '@/generated/graphql';
 
 interface Params extends ParsedUrlQuery {
@@ -33,11 +35,26 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 const PlayerDetailPage: NextPage = () => {
   const router = useRouter();
   const playerId = parseInt(router.query.playerId as string);
+  const queryClient = useQueryClient();
 
   const { isLoading, isError, data } =
     useGetPlayerDetailsQuery<GetPlayerDetailsQuery>({
       playerId,
     });
+
+  const deleteMutation = useDeletePlayerMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries(usePlayersIndexQuery.getKey());
+      queryClient.invalidateQueries(
+        useGetPlayerDetailsQuery.getKey({ playerId })
+      );
+    },
+  });
+
+  const deletePlayerHandler = () => {
+    deleteMutation.mutate({ playerId });
+    router.push('/players');
+  };
 
   {
     isLoading && <h1>Loading...</h1>;
@@ -51,6 +68,7 @@ const PlayerDetailPage: NextPage = () => {
       <h1>Player Details</h1>
       <p>{data!.player.fullName}</p>
       <p>Referred by: {`${data!.player.referredByPlayer?.firstName}`}</p>
+      <button onClick={deletePlayerHandler}>Delete Player</button>
     </>
   );
 };
